85ee1d4a2716ed0f7e545f9cebef0e555ae09637
Marat Abrarov <abrarov@gmail.com>
2021-02-11 15:22:00 +0300

    Explicit signal handler in Java SE application to avoid non-zero exit code for SIGTERM and SIGINT

--- java-se/app/src/main/java/org/mabrarov/javaexitcode/javase/Main.java
+++ java-se/app/src/main/java/org/mabrarov/javaexitcode/javase/Main.java
@@ -2,21 +2,23 @@ package org.mabrarov.javaexitcode.javase;
 
 import java.util.concurrent.CountDownLatch;
 
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
 public class Main {
 
   public static void main(String[] args) {
-    final Runtime runtime = Runtime.getRuntime();
-    final CountDownLatch completed = new CountDownLatch(1);
     final CountDownLatch shutdown = new CountDownLatch(1);
+    final Runtime runtime = Runtime.getRuntime();
     runtime.addShutdownHook(new Thread(() -> {
       System.out.println("Shutdown hook called");
-      shutdown.countDown();
-      await(completed);
     }));
+    NotifierSignalHandler notifierSignalHandler = new NotifierSignalHandler(shutdown);
+    Signal.handle(new Signal("TERM"), notifierSignalHandler);
+    Signal.handle(new Signal("INT"), notifierSignalHandler);
     System.out.println("Press Ctrl+C to exit");
     await(shutdown);
     System.out.println("Main thread is going to stop now");
-    completed.countDown();
   }
 
   private static void await(final CountDownLatch latch) {
@@ -24,6 +26,21 @@ public class Main {
       latch.await();
     } catch (InterruptedException ignored) {
       Thread.currentThread().interrupt();
+    }
+  }
+
+  private static class NotifierSignalHandler implements SignalHandler {
+
+    private final CountDownLatch notifier;
+
+    public NotifierSignalHandler(final CountDownLatch notifier) {
+      this.notifier = notifier;
+    }
+
+    @Override
+    public void handle(final Signal signal) {
+      System.out.println("Signal handler called for " + signal.getName());
+      notifier.countDown();
     }
   }
 
